394b068a8d5f5964fcc16a6fc6b2a931ca1eee85
Tim Zimmermann <tim@linux4.de>
2023-05-20 20:43:40 +0530

    Added eureka bootlogger service
    Tim Zimmermann <tim@linux4.de>

--- bengal.mk
+++ bengal.mk
@@ -129,6 +129,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Debug
+PRODUCT_PACKAGES += eklogger dlopener dlopener.vendor
+
 # Device ID
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
